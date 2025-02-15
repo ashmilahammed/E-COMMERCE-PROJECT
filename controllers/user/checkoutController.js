c7fd3af2505da51e20285a1868069ac735d0562d
Ashmil Ahammed <ashmilahammed0@gmail.com>
2025-02-15 17:53:10 +0530

    upto edit-profile

--- controllers/user/checkoutController.js
+++ controllers/user/checkoutController.js
@@ -465,6 +465,7 @@ const editAddress = async (req, res) => {
         if (!result) {
             return res.status(404).json({ success: false, message: "Address not found" });
         }
+        
 
         return res.status(200).json({ success: true, message: "Address updated successfully" });
     } catch (error) {
